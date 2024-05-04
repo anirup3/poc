b24c26220462de34a3fb93ddd174981058d15d30
anirup3 <mukherjeeanirup@gmail.com>
2024-05-04 17:15:25 +0530

    Landing page : application #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home/home.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent, CommonModule]
 })
 export class AppComponent {
-  title = 'Loan App Citi';
+  title = 'Loan App';
 }
